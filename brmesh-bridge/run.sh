17b0b5e30aafb34aa4d2cbab3930deab82d296a7
Chris Valentino <TofuWeasel@gmail.com>
2025-11-24 18:22:08 -0500

    Zero-config installation - all settings via Web UI - v0.9.14

--- brmesh-bridge/run.sh
+++ brmesh-bridge/run.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-CONFIG_PATH=/data/options.json
-
-export MESH_KEY=$(bashio::config 'mesh_key')
-export MQTT_HOST=$(bashio::config 'mqtt_host')
-export MQTT_PORT=$(bashio::config 'mqtt_port')
-export MQTT_USER=$(bashio::config 'mqtt_user')
-export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
-export DISCOVERY_ENABLED=$(bashio::config 'discovery_enabled')
-
 bashio::log.info "Starting BRMesh Bridge..."
-bashio::log.info "Mesh Key: ${MESH_KEY}"
-bashio::log.info "MQTT: ${MQTT_HOST}:${MQTT_PORT}"
+bashio::log.info "Zero-config start - configure via Web UI"
 
 python3 /brmesh_bridge.py
